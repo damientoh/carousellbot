c2d85686280aba798670739318c6f3e448ed754d
Damien Toh <personal@damienone.com>
2022-12-28 14:28:58 +0700

    disable ssl validate

--- database/database.js
+++ database/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 exports.connectToDb = async () => {
 	try {
 		mongoose.set('strictQuery', true)
-		await mongoose.connect(process.env.DBURL)
+		await mongoose.connect(process.env.DBURL, { sslValidate: false })
 	} catch (error) {
 		throw error
 	}
